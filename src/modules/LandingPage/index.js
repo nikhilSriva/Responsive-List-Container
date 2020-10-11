dcb737d059fe6d97c84c58b16d14f7d4e7ef95c0
nikhilSriva <nikhil21.sri@gmail.com>
2020-10-11 21:14:21 +0530

    - Add Pagination - Add intersection observer

--- src/modules/LandingPage/index.js
+++ src/modules/LandingPage/index.js
@@ -4,28 +4,53 @@ import {withRouter} from 'react-router-dom'
 import CardSection from "../CardSection";
 import {fetchProducts} from "./utils";
 import {SearchBar} from "../../components/SearchBar";
+import {Loader} from "../../components";
 
 class LandingPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: []
+            data: [],
+            page: 1
         }
     }
 
-    fetchData = (query = '') => {
+    fetchData = (query = '', page = 1, clearData) => {
         this.setState({loading: true});
-        fetchProducts(query)
+        fetchProducts(query, page)
             .then(data => {
-                this.setState({data, loading: false})
+                this.setState({data: clearData ? data : [...this.state.data, ...data], loading: false, loadMore: false})
             })
             .catch(e => {
+                this.setState({loading: false, loadMore: false})
             })
     };
 
     componentDidMount() {
-        this.fetchData()
+        this.fetchData();
+        window.scrollTo({
+            left: 0,
+            top: 0
+        })
+        this.setupIntersectionObserver()
     }
+
+    setupIntersectionObserver = () => {
+        let el = document.getElementById('loader');
+        let context = this;
+        let observer = new IntersectionObserver(function (entries) {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    context.setState((state) => ({loadMore: true, page: state.page + 1}), () => {
+                        context.fetchData('', context.state.page)
+                    })
+                }
+            });
+        }, {
+            threshold: 1.0
+        });
+        observer.observe(el);
+    };
 
     render() {
         return (
@@ -34,13 +59,17 @@ class LandingPage extends React.Component {
                     debounce={true}
                     isLoading={this.state.loading}
                     onChangeText={(query) => {
-                        this.fetchData(query);
+                        this.fetchData(query, 1, true);
                     }}
                 />
                 <div className={classes.Container}>
                     {this.state.loading && <div className={classes.Overlay}/>}
                     <CardSection data={this.state.data}/>
                 </div>
+                <div id={'loader'} className={classes.LoadMore}>
+                    {this.state.loadMore && <Loader class={classes.Loader}/>}
+                </div>
+
             </div>
         )
     }
--- src/modules/LandingPage/utils.js
+++ src/modules/LandingPage/utils.js
@@ -1,8 +1,8 @@
 import {AxiosSingleton} from "../../components";
 
-export const fetchProducts = async (query) => {
+export const fetchProducts = async (query, page = 1) => {
     try {
-        let response = await AxiosSingleton.getInstance().get(`https://kpr84j7mce.execute-api.ap-south-1.amazonaws.com/Prod/?query=${query}`)
+        let response = await AxiosSingleton.getInstance().get(`https://kpr84j7mce.execute-api.ap-south-1.amazonaws.com/Prod/?query=${query}&page=${page}`)
         if (response.status === 200)
             return response.data
     } catch (e) {
